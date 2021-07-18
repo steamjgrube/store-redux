9ba417f4b59e5262a06bfd284aaba843541d31e8
Josh Grube <steamjgrube@gmail.com>
2021-07-18 19:32:58 -0400

    save

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://joshg:12345678910@cluster0.ajijt.mongodb.net/StoreDB?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
